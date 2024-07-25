a6e81ad78b7fbb93fd1078e69c7f21c10f6fd6ce
Bananadev3 <angelomodanez30@gmail.com>
2024-07-24 23:42:05 -0300

    close #4 - Modal and Error message

--- js/script.js
+++ js/script.js
@@ -18,22 +18,28 @@ function calc() {
     var output = document.getElementById("output");
     var result = 0;
 
-    output.innerHTML = `</br>Caro <strong>${personName}</strong></br></br></br>`;
-    output.innerHTML += 'Seguem os dados do seu pedido</br></br>';
-    output.innerHTML += 'O seu pedido é: </br></br>';
-
     for (var input of quantities) {
-        if (input.value == 0) {
-            continue;
+        if (input.value > 0) {
+            var value = prods[input.id - 1].price * parseFloat(input.value);
         }
-
-        var id = input.id;
-        var total = prods[id - 1].price * parseFloat(input.value);
-
-        output.innerHTML += `<li>Prato: ${prods[id - 1].name} - Preço unitário: ${formatter.format(prods[id - 1].price)} - Quantidade: ${input.value} - Total: ${formatter.format(total)}</li>`;
-
-        result += total;
     }
 
-    output.innerHTML += `</br><h4>Preço final: ${formatter.format(result)}</h4>`;
+    if (value > 0) {
+        output.innerHTML = `Caro <strong>${personName}</strong>, seguem os dados do seu pedido:</br></br>`;
+
+        for (var input of quantities) {
+            if (input.value == 0) {
+                continue;
+            }
+            var total = prods[input.id - 1].price * parseFloat(input.value);
+            output.innerHTML += `<li>Prato: ${prods[input.id - 1].name} - Preço unitário: ${formatter.format(prods[input.id - 1].price)} - Quantidade: ${input.value} - Total: ${formatter.format(total)}</li>`;
+
+            result += total;
+        }
+        output.innerHTML += `</br></br><h2 class="text-success">Preço final: ${formatter.format(result)}</h2>`;
+    }
+
+    else {
+        output.innerHTML += `<h1 class="text center fs-3 fw-bold text-danger">Caro <strong>${personName}</strong>, SELECIONE UM PRATO</h1>`;
+    }
 }
